3df8f8b73183947acb49d23b08d9918f19e27ddb
David Rook <hotei1352@gmail.com>
2015-05-10 14:13:49 -0400

    added PermutedInts and Factorial

--- mdr_intHelp.go
+++ mdr_intHelp.go
@@ -2,6 +2,7 @@ package mdr
 
 import (
 	"fmt"
+	"log"
 	"net"
 )
 
@@ -43,7 +44,81 @@ func (a Ints) ContainsI(b int) bool {
 	return false
 }
 
-// ==================   nexit are the 32 bit functions =====================
+// RotT2H rotates the head of an array to tail position
+//	abcd => bcda
+func (a Ints) RotH2T() Ints {
+	ln := len(a)
+	if ln <= 1 {
+		return a
+	}
+	return append(a[1:], a[0])
+}
+
+//
+// a b c  ln = 3
+// 0 1 2
+// c a b
+
+// RotT2H rotates the tail of an array to head position
+//	abcd => dabc
+func (a Ints) RotT2H() Ints {
+	ln := len(a)
+	if ln <= 1 {
+		return a
+	}
+	var x Ints = Ints{a[ln-1]}
+	return append(x, a[:ln-1]...)
+	//
+	//return
+}
+
+// PermutedInts returns all the permutaions of the original array
+// length of array must be in range of [0..6]
+func PermutedInts(a Ints) []Ints {
+	ln := len(a)
+	if ln <= 1 {
+		return []Ints{a}
+	}
+	if ln == 2 {
+		return []Ints{{a[0], a[1]}, {a[1], a[0]}}
+	}
+	if ln > 6 {
+		log.Panicf("permute array length > 6 (test phase only)\n")
+	}
+	expLen := Factorial(len(a))
+	//fmt.Printf("PermutedInts should return %d elements each with %d elements\n", expLen, len(a))
+
+	// make the empty return value array with zero length but full capacity
+	var rv = make([]Ints, 0, Factorial(len(a)))
+	for i := 0; i < ln; i++ {
+		var x = a
+		y := PermutedInts(x[1:]) // permute the tail
+		for _, p := range y {
+			x = append(Ints{a[0]}, p...)
+			rv = append(rv, x)
+		}
+		a = a.RotH2T()
+		//fmt.Printf("rotated a = %v\n", a)
+	}
+	if len(rv) != expLen {
+		log.Panicf("return array has wrong length\n")
+	}
+	return rv
+}
+
+// BUG(mdr) Factorial using recursion may not be the best choice ...
+// BUG(mdr) check for overflow in Factorial or limit choice of n to good range
+func Factorial(n int) int {
+	if n < 0 {
+		return -1
+	}
+	if n <= 1 {
+		return 1
+	}
+	return n * Factorial(n-1)
+}
+
+// ==================   next are the 32 bit functions =====================
 
 func ThirtyTwoNet(n []byte) uint32 {
 	return Uint32FromMSBytes(n)
@@ -84,8 +159,8 @@ func ThirtyTwoBit(n []byte) uint32 {
 }
 
 // returns a uint32 from IPv4 so we can use as index to map
-//    - beware magic numbers
-//    - presumes knowledge of net.IP internals order
+//    - beware - there are magic numbers here that
+//    - presume knowledge of net.IP internals order
 func Uint32FromIP(ip net.IP) uint32 {
 	if false {
 		for ndx, value := range ip {
@@ -193,7 +268,7 @@ func AbsInt64(a int64) int64 {
 	return a
 }
 
-// convert an int64 into an 8 byte array, LSB first (littleEndian)
+// convert an int64 into an 8 byte array, LSB first (LittleEndian)
 // test is Test_011
 func LSBytesFromInt64(n int64) []byte {
 	rv := make([]byte, 0, 8)
@@ -204,7 +279,7 @@ func LSBytesFromInt64(n int64) []byte {
 	return rv
 }
 
-// convert int64 to [0:8]byte slice with MSB first
+// convert int64 to [0:8]byte slice with MSB first (BigEndian)
 // reverse function is
 // test is Test_013
 func MSBytesFromInt64(n int64) []byte { //
@@ -228,7 +303,7 @@ func MSBytesFromInt64(n int64) []byte { //
 }
 
 // convert an 8 byte slice (BigEndian - MSB First) into an int64
-// Test is Test_014
+// see Test_014
 func Int64FromMSBytes(b []byte) int64 {
 	if len(b) != 8 {
 		FatalError(fmt.Errorf("mdr: Slice must be exactly 8 bytes\n"))
@@ -253,7 +328,7 @@ func Int64FromMSBytes(b []byte) int64 {
 }
 
 // convert an 8 byte array into an int64
-// Test is Test_012
+// see Test_012
 func Int64FromLSBytes(b []byte) int64 {
 	if len(b) != 8 {
 		FatalError(fmt.Errorf("mdr: Slice must be exactly 8 bytes\n"))
